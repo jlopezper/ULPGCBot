ef4e61c5ddeeeec54267d23413b40ae549251c46
jlopezper <jorge.loper@gmail.com>
2018-05-09 22:35:30 +0200

    First commit

---
+++ R/01_web_scrapping.R
@@ -0,0 +1,22 @@
+#' Function for scrapping data from the ULPGC webpage
+#' This function scraps the last news from the ULPGC news section page
+#'
+#' @param url URL from the ULPGC news section
+#'
+#' @return String with last news
+#'
+#' @examples
+#' ulpgc_scrap()
+#'
+#' @export
+
+
+ulpgc_scrap <- function(url = "https://www.ulpgc.es/noticias") {
+
+  current <-  xml2::read_html(url) %>%
+    rvest::html_nodes(".views-row-even .field-content") %>%
+    rvest::html_text() %>%
+    dplyr::nth(2)
+
+  current
+}
